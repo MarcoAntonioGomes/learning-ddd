ae9a795a6bad94f6ae268bb7b5d79775dc158eff
marco <marco@joinder.me>
2022-10-23 13:22:50 -0300

    Understanding business classes with behavior

--- src/costumer.ts
+++ src/costumer.ts
@@ -1,5 +1,26 @@
+//Changed the anemic class to fit to the business rules
+
 class Customer {
-  _id: string;
-  _name: string;
-  _address: string;
+  private id: string;
+  private name: string;
+  private address: string;
+  private active: boolean = true;
+
+  constructor(id: string, name: string, address: string) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+  }
+
+  changeName(name: string) {
+    this.name = name;
+  }
+
+  activate() {
+    this.active = true;
+  }
+
+  desactive() {
+    this.active = false;
+  }
 }
